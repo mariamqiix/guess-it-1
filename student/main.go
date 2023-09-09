1bdbf34a389682e7f7c5ec39dd8f91f275ac9095
maabbas <mx38x@iclcoud.com>
2023-09-10 02:53:04 +0300

    add range

--- student/main.go
+++ student/main.go
@@ -16,15 +16,18 @@ func main() {
 	for scanner.Scan() {
 		currentY, _ := strconv.Atoi(scanner.Text())
 		x++
+		if currentY < 20 {
+			currentY = 100
+		} else if currentY < 600 {
+			currentY = 200
+		}
 		array = append(array, currentY)
 		firstY := array[0]
 		y := (((currentY - firstY) / x) * x) + firstY
 		Vari := Variance(array,x)
 		Yrange := (int(Vari) / 10)
 		fmt.Printf("%d %d\n", (y - (Yrange )), (y + (Yrange )))
-		// fmt.Print()
-		// fmt.Print(" - ")
-		// fmt.Println()
+
 	}
 }
 
