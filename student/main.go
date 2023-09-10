3073d0fbc069debb2df995aab7b48aa3b6fb3e54
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-09-10 19:35:27 +0000

    Update 'student/main.go'

--- student/main.go
+++ student/main.go
@@ -1,12 +1,13 @@
 package main
+
 import (
 	"bufio"
 	"fmt"
-	// "math"
+	"math"
 	"os"
-	// "sort"
 	"strconv"
 )
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	x := 0
@@ -14,19 +15,18 @@ func main() {
 	for scanner.Scan() {
 		currentY, _ := strconv.Atoi(scanner.Text())
 		x++
-		if currentY < 20 {
+		if currentY < 50 {
 			currentY = 100
-		} else if currentY > 600 {
+		} else if currentY > 350 {
 			currentY = 200
 		}
 		array = append(array, currentY)
-		firstY := array[0]
-		y := (((currentY - firstY) / x) * x) + firstY
-		Vari := Variance(array,x)
-		Yrange := (int(Vari) / 10)-30
-		fmt.Printf("%d %d\n", (y -3 - (Yrange/50)), (y +3 + (Yrange/50)))
+		y := math.Round(ava(array))
+		standard := math.Round(StandardDeviation(array))
+		fmt.Printf("%d %d\n", int((y - standard)), int((y + standard)))
 	}
 }
+
 func ava(i []int) float64 {
 	z := 0.0
 	for x := 0; x < len(i); x++ {
@@ -34,30 +34,20 @@ func ava(i []int) float64 {
 	}
 	return (z / float64(len(i)))
 }
-func Variance(i []int,n int) float64 {
+
+func Variance(i []int) float64 {
 	DeltaX := ava(i)
 	TheX := 0.0
 	Variance := 0.0
-	if n < 10 {
 	for x := 0; x < len(i); x++ {
 		z := (float64(i[x]) - float64(DeltaX))
 		TheX += (z * z)
 	}
 	Variance = TheX / (float64(len(i)))
-	} else {
-		for x := 0 ; x < 10 ; x++ {
-			z := (float64(i[x]) - float64(DeltaX))
-			TheX += (z * z)
-		}
-		Variance = TheX / (float64(10))
-
-	}
 	return Variance
 }
 
-
-
-func printError(err error) {
-	fmt.Println("ERROR: " + err.Error())
-	os.Exit(1)
+func StandardDeviation(i []int) float64 {
+	x := Variance(i)
+	return (math.Sqrt(x))
 }
